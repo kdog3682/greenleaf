9b82e535f65263899ab9df6ab8e564895fcafd1f
kdog3682 <kdog3682@gmail.com>
2024-05-25 20:00:47 -0400

    organize

---
+++ src/clipper.js
@@ -0,0 +1,5 @@
+
+import { appendVariableFile, writeUnitTest, read, clip2, appendVariable2, clip, appendVariable, write, rpw, isFile, sysget, } from "/home/kdog3682/2023/node-utils.js"
+export {
+    clip as clipper
+}
